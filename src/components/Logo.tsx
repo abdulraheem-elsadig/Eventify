44c71f45888eb1c3bc07a10811e3c7d3f504d3bb
abdulraheem elsadig <abdulraheem.elsadig@gmail.com>
2025-05-03 17:15:01 +0400

    feat: initialize project structure

---
+++ src/components/Logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <div className="relative flex items-center justify-center h-14 w-fit gap-1">
+        <span className="font-bold text-3xl tracking-tighter">Eventify</span>
+        <div className="relative size-8 overflow-hidden">
+          <Image
+            src="/favicon/favicon.ico"
+            alt="logo"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
+import Logo from "@/components/Logo";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      <div className="container">
+        <Logo />
+        <Component {...pageProps} />;
+      </div>
+    </>
+  );
 }
